1b6a871aac1411b94586c4036bd9bdaf903e1b06
Michael McCoy <mccoymichaeljohn@gmail.com>
2019-11-25 14:34:24 -0500

    first commit

---
+++ colors.js
@@ -0,0 +1,12 @@
+module.exports = {
+  background: '#FFFFFF',
+  backgroundDark: '#EDFFDE',
+
+  primary: '#FF8000',
+  primaryLight: '#FFCF9E',
+  primaryDark: '#FF8000',
+
+  secondary: '#56BF00',
+  secondaryLight: '#A2FF57',
+  secondaryDark: '#449600',
+};
